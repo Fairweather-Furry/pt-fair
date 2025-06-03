dd4e358b068d395abc16f35b848833fb5bf437f4
BowserCrusher <bowsercrusher@outlook.com>
2025-06-03 16:44:32 -0700

    Added all John Gutter checks

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -119,3 +119,66 @@ function john_gutter_pin()
     (has("op_noise") and has("op_diff_norm") and has("sjump")) or
   )
 end
+
+function john_gutter_s1()
+  return(
+    (has("op_pepp") and has("op_diff_exp") and (has("sjump") or has("wclimb") or has("ucut") or has("grab") or has("bodyslam"))) or
+    (has("op_pepp") and has("op_diff_norm") and (has("sjump") or has("wclimb") or has("ucut"))) or
+    (has("op_noise") and has("op_diff_exp") and (has("sjump") or has("ucut") or has("crush") or has("wbounce") or has("grab") or has("bodyslam"))) or
+    (has("op_noise") and has("op_diff_norm") and (has("sjump") or has("wbounce") or has("crush") or has("ucut"))) or
+  )
+end
+
+function john_gutter_s2()
+  return(
+    (has("op_pepp") and has("op_diff_exp") and (has("sjump") or has("wclimb"))) or
+    (has("op_pepp") and has("op_diff_norm") and has("sjump") and has("bodyslam")) or
+    (has("op_noise") and has("op_diff_exp") and (has("sjump") or has("ucut") or has("crush") or has("wbounce"))) or
+    (has("op_noise") and has("op_diff_norm") and has("sjump") and (has("bodyslam") or has("crush") or has("nado"))) or
+  )
+end
+
+function john_gutter_s3()
+  return(
+    (has("op_pepp") and has("op_diff_exp") and (has("sjump") or has("wclimb"))) or
+    (has("op_pepp") and has("op_diff_norm") and has("sjump")) or
+    (has("op_noise") and has("op_diff_exp") and (has("sjump") or has("ucut") or has("crush") or has("wbounce"))) or
+    (has("op_noise") and has("op_diff_norm") and has("sjump")) or
+  )
+end
+
+function john_gutter_tre()
+  return(
+    (has("op_pepp") and has("op_diff_exp") and (has("sjump") or has("wclimb"))) or
+    (has("op_pepp") and has("op_diff_norm") and has("sjump")) or
+    (has("op_noise") and has("op_diff_exp") and (has("sjump") or has("ucut") or has("crush") or has("wbounce"))) or
+    (has("op_noise") and has("op_diff_norm") and has("sjump")) or
+  )
+end
+
+function john_gutter_ct1()
+  return(
+    (has("op_pepp") and has("op_diff_exp") and (has("sjump") or has("wclimb"))) or
+    (has("op_pepp") and has("op_diff_norm") and has("sjump")) or
+    (has("op_noise") and has("op_diff_exp") and (has("sjump") or has("ucut") or has("crush") or has("wbounce"))) or
+    (has("op_noise") and has("op_diff_norm") and has("sjump")) or
+  )
+end
+
+function john_gutter_ct2()
+  return(
+    (has("op_pepp") and has("op_diff_exp") and (has("sjump") or has("wclimb"))) or
+    (has("op_pepp") and has("op_diff_norm") and has("sjump")) or
+    (has("op_noise") and has("op_diff_exp") and (has("sjump") or has("ucut") or has("crush") or has("wbounce"))) or
+    (has("op_noise") and has("op_diff_norm") and has("sjump")) or
+  )
+end
+
+function john_gutter_ct3()
+  return(
+    (has("op_pepp") and has("op_diff_exp") and (has("sjump") or has("wclimb"))) or
+    (has("op_pepp") and has("op_diff_norm") and has("sjump") and has("mach4")) or
+    (has("op_noise") and has("op_diff_exp") and (has("sjump") or has("ucut") or has("crush") or has("wbounce"))) or
+    (has("op_noise") and has("op_diff_norm") and has("sjump")) or
+  )
+end
